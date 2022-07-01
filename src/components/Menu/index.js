5d4823dde51ef2844e5539f33f489bfbe01af180
JulioNelVV <julionelvasquezvanegas@gmail.com>
2022-06-30 19:57:17 -0500

    menu custom hook added

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,97 +1,55 @@
-import { useContext } from 'react';
-import { useEffect, useRef, useState } from 'react';
-import { Link, useLocation } from 'wouter';
-import globalContext from '../../context/globalContext';
-import useFetch from '../../hooks/useFetch';
-import style from './style.module.css'
-function Menu({}){
-    const [visible, setVisible]=useState(false);
-    const [movie, setMovie]=useState("");
-    const [location, setLocation]=useLocation();
-    const {data, isLoading, error}=useFetch("https://api.themoviedb.org/3/genre/movie/list?api_key=583ad481a868c7cb43cca20c20a9d9c2");
-    const {setCurrentGenre, setSliderDisplay}=useContext(globalContext);
-    const menuList=useRef();
-    const onClickHandler=(genre)=>{
-        setCurrentGenre({...genre});
-        setSliderDisplay("flex");
-        setVisible(!visible);
-    }
-    const onChangeHanlder=(e)=>{
-        setMovie(e.target.value)
-    }
-    const onSubmitHandler=(e)=>{
-        e.preventDefault();
-        setLocation(`/search/${movie}/${1}`);
-        setSliderDisplay("none");
-        setMovie("");
-        setVisible(!visible);
-    }
-    useEffect(()=>{
-        if(!isLoading){
-            if(visible){
-                menuList.current.classList.add(style["menu-list--show"]);
-                menuList.current.classList.remove(style["menu-list--hide"]);
-            }else{
-                menuList.current.classList.remove(style["menu-list--show"]);
-                menuList.current.classList.add(style["menu-list--hide"]);
-                setTimeout(()=>{
-                    menuList.current.classList.remove(style["menu-list--hide"]);
-                },800)
-            }
-        }
-        
-    },[visible])
-    let submenu;
+import { Link } from 'wouter';
+import useMenu from '../../hooks/useMenu';
+import style from './style.module.css';
+
+function Menu(){
+    //Menu custom hook
+    let {
+        isLoading,
+        submenu, 
+        visible,
+        menuList,
+        movie,
+        setMenuListVisibility,
+        onSubmitHandler,
+        onChangeHandler,
+    }=useMenu();
+    //Component rendering
     if(!isLoading){
-        
-        if(error!==null){
-            submenu=<li>Error: {` ${error.error} ${error.description||"Failed to Fetch"}`}</li>
-        }else{
-            submenu= data.genres.map((genre, index)=>{
-                return(
-                    <li
-                        key={genre.id}
-                        className={style["submenu__item"]}
-                    >
-                        <Link  onClick={()=>onClickHandler(genre)} to={`/genre/${genre.name}/${1}`}>
-                            <a className={style['submenu__link']}>{genre.name}</a>
-                        </Link>
-                        
-                    </li>
-                ) 
-            })
-        }
         return(
             <nav className={style['menu']}>
                 <div 
                     className={`${style["menu-button-wrapper"]} ${style[`menu-button-wrapper--${visible?"cross":"hamburguer"}`]}`}
-                    onClick={()=>{
-                        setVisible(!visible);
-                    }}
+                    onClick={setMenuListVisibility}
                 >
-                    <button className={style["menu__button"]}>
-                    </button>
+                    <button className={style["menu__button"]}></button>
                 </div>
-              
-                <ul ref={menuList} className={style["menu-list"]}>
+                <ul
+                    ref={menuList}
+                    className={style["menu-list"]}
+                >
                     <li className={style['menu-list__item']}>
-                        <form className={style['search-form']} onSubmit={onSubmitHandler}>
+                        <form
+                            className={style['search-form']}
+                            onSubmit={onSubmitHandler}
+                        >
                             <input type="text"
                                 className={style['search-form__text']}
                                 placeholder='search movie...'
-                                onChange={onChangeHanlder}
+                                onChange={onChangeHandler}
                                 value={movie}
                             />
-                            <input type="button" className={style['search-form__button']}/>
+                            <input 
+                                type="button"
+                                className={style['search-form__button']}/
+                            >
                         </form>
-                    </li>
-                   
+                    </li> 
                     <li className={style['menu-list__item']}>
-                        <Link onClick={()=>{
-                            setSliderDisplay("flex")
-                            setVisible(!visible)
-                                }
-                            }to='/home/1'>
+                        <Link
+                            onClick={setMenuListVisibility}
+                            to='/home/1'
+                        >
                             <a className={style['menu-list__link']}>Home</a>
                         </Link>
                         
@@ -102,14 +60,11 @@ function Menu({}){
                             {
                                 submenu
                             }
-                            
                         </ul>
                     </li>
                 </ul>
-            </nav>
-           
+            </nav>      
         )
     }
-    
 }
 export default  Menu;
---
+++ src/hooks/useMenu.js
@@ -0,0 +1,83 @@
+import { useContext, useEffect, useRef, useState } from 'react';
+import { Link, useLocation } from 'wouter';
+import globalContext from '../context/globalContext';
+import useFetch from './useFetch';
+import style from "../components/Menu/style.module.css"
+
+const useMenu=()=>{
+    const menuList=useRef();
+    const API_KEY="583ad481a868c7cb43cca20c20a9d9c2";
+    const URL=`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+    let submenu;
+    //Controls visibility of the menu list
+    const [visible, setVisible]=useState(false);
+    //Movie that is being search in the form
+    const [movie, setMovie]=useState("");
+    const [location, setLocation]=useLocation();
+    const {data, isLoading, error}=useFetch(URL);
+    const {setCurrentGenre, setSliderDisplay}=useContext(globalContext);
+    
+    const setMenuListVisibility=()=>{
+        setVisible(!visible);
+    }
+    const onClickGenre=(genre)=>{
+        setCurrentGenre({...genre});
+        setSliderDisplay("flex");
+        setMenuListVisibility();
+    }
+    const onChangeHandler=(e)=>{
+        setMovie(e.target.value)
+    }
+    const onSubmitHandler=(e)=>{
+        e.preventDefault();
+        setLocation(`/search/${movie}/${1}`);
+        setSliderDisplay("none");
+        setMovie("");
+        setMenuListVisibility();
+    }
+
+    useEffect(()=>{
+        if(!isLoading){
+            if(visible){
+                menuList.current.classList.add(style["menu-list--show"]);
+                menuList.current.classList.remove(style["menu-list--hide"]);
+            }else{
+                menuList.current.classList.remove(style["menu-list--show"]);
+                menuList.current.classList.add(style["menu-list--hide"]);
+                setTimeout(()=>{
+                    menuList.current.classList.remove(style["menu-list--hide"]);
+                },800)
+            }
+        }
+    },[visible])
+    if(!isLoading){
+        if(error!==null){
+            submenu=<li>Error: {` ${error.error} ${error.description||"Failed to Fetch"}`}</li>
+        }else{
+            submenu= data.genres.map((genre)=>{
+                return(
+                    <li
+                        key={genre.id}
+                        className={style["submenu__item"]}
+                    >
+                        <Link  onClick={()=>onClickGenre(genre)} to={`/genre/${genre.name}/${1}`}>
+                            <a className={style['submenu__link']}>{genre.name}</a>
+                        </Link>
+                        
+                    </li>
+                ) 
+            })
+        }
+    }
+    return{
+        isLoading,
+        submenu, 
+        visible,
+        menuList,
+        movie,
+        setMenuListVisibility,
+        onSubmitHandler,
+        onChangeHandler,
+    }
+}
+export default useMenu;
