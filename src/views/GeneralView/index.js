b5d8cc28a0f1e3964a32558fbf8e48abc628aeec
JulioNelVV <julionelvasquezvanegas@gmail.com>
2022-07-02 14:48:09 -0500

    h1 added

--- src/views/GeneralView/index.js
+++ src/views/GeneralView/index.js
@@ -5,15 +5,21 @@ import globalContext from "../../context/globalContext";
 function GeneralView({...props}){
     const {params}=props;
     const {sliderDisplay, setSliderDisplay}=useContext(globalContext);
-    
+    let currentParam=Object.keys(params)[0];
+    const defaultTitle="Most popular movies";
+    const viewTitle={
+        "keyword": `search results for ${params.keyword}`,
+        "genre_name": `${params.genre_name}`
+    }
     useEffect(()=>{
         setSliderDisplay("none");
-        if(Object.keys(params)[0]!=="keyword"){
+        if(currentParam!=="keyword"){
             setSliderDisplay("flex");
         }
     },[params])
     return(
         <div className={`view view--slider-${sliderDisplay}`}>
+            <h1>{viewTitle[currentParam]||defaultTitle}</h1>
             <MoviesGrid params={params}/>            
         </div>
 
